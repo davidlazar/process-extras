0846efb9798d8d038a840b2929861ca8fda7ffc1
David Lazar <lazar6@illinois.edu>
2012-08-12 14:14:43 -0500

    Add support for ByteString.Lazy.

---
+++ src/System/Process/ByteString/Lazy.hs
@@ -0,0 +1,52 @@
+module System.Process.ByteString.Lazy where
+
+import Control.Concurrent
+import Control.Monad
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as B
+import System.Process
+import System.Exit (ExitCode)
+import System.IO
+
+-- | Like 'System.Process.readProcessWithExitCode', but using 'ByteString'
+readProcessWithExitCode
+    :: FilePath                 -- ^ command to run
+    -> [String]                 -- ^ any arguments
+    -> ByteString               -- ^ standard input
+    -> IO (ExitCode, ByteString, ByteString) -- ^ exitcode, stdout, stderr
+readProcessWithExitCode cmd args input = do
+    (Just inh, Just outh, Just errh, pid) <-
+        createProcess (proc cmd args){ std_in  = CreatePipe,
+                                       std_out = CreatePipe,
+                                       std_err = CreatePipe }
+    outMVar <- newEmptyMVar
+    outM <- newEmptyMVar
+    errM <- newEmptyMVar
+
+    -- fork off a thread to start consuming stdout
+    _ <- forkIO $ do
+        out <- B.hGetContents outh
+        putMVar outM out
+        putMVar outMVar ()
+
+    -- fork off a thread to start consuming stderr
+    _ <- forkIO $ do
+        err  <- B.hGetContents errh
+        putMVar errM err
+        putMVar outMVar ()
+
+    -- now write and flush any input
+    when (not (B.null input)) $ do B.hPutStr inh input; hFlush inh
+    hClose inh -- done with stdin
+
+    -- wait on the output
+    takeMVar outMVar
+    takeMVar outMVar
+    hClose outh
+
+    -- wait on the process
+    ex <- waitForProcess pid
+    out <- readMVar outM
+    err <- readMVar errM
+
+    return (ex, out, err)
